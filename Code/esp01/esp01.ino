44b0b1dbf1fbafd530f3feff7eac10b58d665fc2
matteusch <matteuschger@gmail.com>
2026-02-27 02:24:05 +0100

    WiFi connection and Telegram communication via ESP-01

---
+++ Code/esp01/esp01.ino
@@ -0,0 +1,88 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+#include <ArduinoJson.h>
+
+// WiFi and Telegram data
+const char* ssid = "#";
+const char* password = "#";
+const char* BOT_TOKEN = "#";
+const char* CHAT_ID = "#";
+
+// Setup a secure connection for Telegram
+WiFiClientSecure secured_client;
+UniversalTelegramBot bot(BOT_TOKEN, secured_client);
+
+// Read from chat constants
+int botRequestDelay = 1000; // Check for messages every 1 second
+unsigned long lastTimeBotRan = 0;
+
+void setup() {
+  // Init serial
+  Serial.begin(115200);
+  delay(1000);
+
+// Check for wifi connection
+
+  Serial.println("Attempting to connect to WiFi:");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  secured_client.setInsecure();
+
+  bot.sendMessage(CHAT_ID, "The device is online", "");
+
+
+
+}
+//Poll for new messages
+void loop() {
+  if (millis() - lastTimeBotRan > botRequestDelay) {
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+
+    while (numNewMessages) {
+      Serial.println("New message detected.");
+      handleNewMessages(numNewMessages);
+      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    }
+    
+    lastTimeBotRan = millis();
+  }
+}
+
+//Function that reads chat contents
+void handleNewMessages(int numNewMessages) {
+  for (int i = 0; i < numNewMessages; i++) {
+    
+    String chat_id = bot.messages[i].chat_id;
+    
+    String text = bot.messages[i].text;
+
+    Serial.println("Message: " + text);
+
+    if (text == "/on") {
+      Serial.println("Command: ON");
+      bot.sendMessage(chat_id, "Turning PC on...");
+    }
+    
+    else if (text == "/status") {
+      Serial.println("Command: STATUS");
+      bot.sendMessage(chat_id, "System online");
+    }
+
+    else if (text == "/off"){
+      Serial.println("Command: OFF");
+      bot.sendMessage(chat_id, "Turning PC off...");
+    }
+
+    else{
+      bot.sendMessage(chat_id, "Unknown command. Try: /on, /off, /status");
+    }
+  }
+}
